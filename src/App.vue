077ba9a0a582831e1929f8775cefa3ca05527af6
marvelousness <marvelousness@foxmail.com>
2020-03-16 18:34:24 +0800

    将 1.0.0 合并到主干

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
 	<div>
-		<h1>Hello Vue!</h1>
-		<p><img src="~/statics/images/logo.jpg" />{{author}}</p>
-		<p>{{url}}</p>
+	
+	<Card class="card">
+		<h1 slot="title">Hello Vue!</h1>
+		<a href="#" slot="extra" @click.prevent="handleExtraClick">say hi to me!</a>
+	
+		<p><img src="~/statics/images/logo.jpg" /><a :href="'mailto:' + author">{{author}}</a></p>
+		<p><a :href="url" target="_blank">{{url}}</a></p>
+	</Card>
+	
 	</div>
 </template>
 
@@ -13,13 +19,26 @@
 				author: "marvelousness@foxmail.com",
 				url: "https://github.com/marvelousness/vue-view-design"
 			}
+		},
+		methods: {
+			handleExtraClick: function() {
+				this.$Message.info("Hi");
+			}
 		}
 	}
 </script>
 
 <style>
+	.card {
+		width:350px;
+		margin: 50px auto 0px auto;
+	}
+	.card p {
+		margin: 10px 0px;
+	}
 	p>img {
 		width: 2em;
 		vertical-align: middle;
+		margin-right: 1em;
 	}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,25 +1,14 @@
-// 导入Vue框架
 import Vue from "vue";
 import App from "./App";
+import view from "view-design";
 // import "es6-promise/auto";
-// import router from "./router";
-// import store from "./vuex-store";
-// import prototypes from "./prototypes";
-// import VueCookies from "huofutp-vue-cookies";
 
-// 引入接口
-// import "api";
-
-// 导入前端指令
-// import "./client/directive";
 
 // 引入系统主题
-// import "view-element-3/dist/styles/iview.css";
-// import VE from "view-element-3";
+import "view-design/dist/styles/iview.css";
 
 Vue.config.devtools = true;
 
-// Vue.use(prototypes);
-// Vue.use(VueCookies);
+Vue.use(view);
 
 new Vue(App).$mount("#app");
